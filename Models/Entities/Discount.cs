55bd3537d78808896e237298fedcbc9da1deb8c5
Marcin Binkowski <marcin.binkowski@onet.eu>
2024-11-21 21:37:29 +0000

    a

---
+++ Models/Entities/Discount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PDAB.Models;
+
+public partial class Discount
+{
+    public int DiscountId { get; set; }
+
+    public string? DiscountName { get; set; }
+
+    public decimal DiscountPercentage { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<DiscountProduct> DiscountProducts { get; set; } = new List<DiscountProduct>();
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+}
---
+++ Models/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PDAB.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public int ManufacturerId { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+
+    public virtual ICollection<DiscountProduct> DiscountProducts { get; set; } = new List<DiscountProduct>();
+
+    public virtual Manufacturer Manufacturer { get; set; } = null!;
+
+    public virtual ICollection<ProductImage> ProductImages { get; set; } = new List<ProductImage>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
