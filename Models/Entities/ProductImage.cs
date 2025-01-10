cfce7b7f5a93daad38c5830a7b16f6304dda264b
Marcin Binkowski <marcin.binkowski@onet.eu>
2025-01-10 12:26:23 +0000

    a

---
+++ Models/Entities/ProductImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PDAB.Models;
+
+public partial class ProductImage
+{
+    public int ProductImageId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public string ImageUrl { get; set; } = null!;
+
+    public virtual Product Product { get; set; } = null!;
+}
